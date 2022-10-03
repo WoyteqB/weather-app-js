8b757008aa3b1ae1c94f86b1dadfe580a4374e44
wojtek.brzoska@gmail.com <grafik@up.local>
2022-10-03 15:55:17 +0200

    Wstepna struktura projektu 2

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ let showCityWeather = (city) =>{
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=f0c515d63a64608d97e703d0f0121976&lang=pl&units=metric ')
     .then( (res) => res.json())
     .then( (data) => console.log(data));
-
 };
 
 searchButton.addEventListener("click", ()=>{
