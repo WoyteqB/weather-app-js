ea6fffc10c277c81ce332cf0cb2e40fb9d0676f8
Wojtek Brzoska <wojtek.brzoska@gmail.com>
2022-10-09 19:59:49 +0200

    Zmiana API na OpenWeatherMap

--- app.js
+++ app.js
@@ -3,23 +3,35 @@ const searchButton = document.querySelector("header button.search");
 const closeAutosearchButton = document.querySelector("header button.close");
 const displayWeather = document.querySelector("main .wrapper");
 const searchWrap = document.querySelector(".search-wrap");
+const autocomplete = document.querySelector(".search-wrap .input-autocomplate ul")
 
-let showCityWeather = (city) =>{
+const cityName = document.querySelector(".weather-city .city h2");
+const cityDate = document.querySelector(".weather-city .first-line .date");
+const cityDayStart = document.querySelector(".weather-city .first-line .sun .day-start");
+const cityDayEnd = document.querySelector(".weather-city .first-line .sun .day-end");
+const cityIcon = document.querySelector(".weather-city .secound-line .icon img");
+const cityTemp = document.querySelector(".weather-city .secound-line .temp span");
+const cityDesc = document.querySelector(".weather-city .secound-line .details .desc");
+const cityTempFeel = document.querySelector(".weather-city .secound-line .details .feel .value");
+const cityPressure = document.querySelector(".weather-city .secound-line .details .pressure .value");
+const cityWind= document.querySelector(".weather-city .secound-line .details .wind .value");
+
+let showCityWeather = (cityId) =>{
     let errorTitle = "";
     let errorText = "";
+    let city;
     fetch(
-        'http://dataservice.accuweather.com/locations/v1/cities/autocomplete?q='+
-        city+'&apikey=stvXgfCdwJ2XzNAR0N6fppcGvPg4Sez0&language=pl-PL'
+        'https://api.openweathermap.org/data/2.5/weather?q='+cityId+'&lang=pl&appid=f0c515d63a64608d97e703d0f0121976&units=metric'
         )
     .then( res => {
         console.log(res);
         if(res.ok ){
             return res
         }else {
-            errorTitle = "Błąd "+ res.status;
-            errorText = res.statusText;
+            errorTitle = "Błąd "+ res.Code;
+            errorText = res.Message;
         }
-        throw Error(res.status + " - Połączenie")
+        throw Error(res)
     })
     .then( (res) => {
         console.log("JSON");
@@ -30,50 +42,88 @@ let showCityWeather = (city) =>{
         console.log("DATA");
         console.log(data)
         //setDisplayWeather(data);
+        setCityDetails(data);
     })
     .catch(err => {
         displayError(errorTitle, errorText)
     });
+    
 };
 
-let showCitylist = (city) =>{
-    let errorTitle = "";
-    let errorText = "";
-    fetch(
-        'http://dataservice.accuweather.com/locations/v1/cities/autocomplete?q='+
-        city+'&apikey=stvXgfCdwJ2XzNAR0N6fppcGvPg4Sez0&language=pl-PL'
-        )
-    .then( res => {
-        //console.log(res);
-        if(res.ok ){
-            return res
-        }else {
-            errorTitle = "Błąd "+ res.status;
-            errorText = res.statusText;
-        }
-        throw Error(res.status + " - Połączenie")
-    })
-    .then( (res) => {
-        //console.log("JSON");
-        return res.json();
-    })
-    .then( (data) => {
-        //console.log(data, displayWeather);
-        //console.log("DATA");
-        //console.log(data)
-        data.forEach(element => {
-            console.log(element.LocalizedName+", "+element.AdministrativeArea.LocalizedName);
-        });
-        //setDisplayWeather(data);
-    })
-    .catch(err => {
-        displayError(errorTitle, errorText)
-        console.log(err);
-    });
-};
+// let showCitylist = (city) =>{
+//     let errorTitle = "";
+//     let errorText = "";
+//     // fetch(
+//     //     'http://dataservice.accuweather.com/locations/v1/cities/autocomplete?q='+
+//     //     city+'&apikey=stvXgfCdwJ2XzNAR0N6fppcGvPg4Sez0&language=pl-PL'
+//     //     , {mode: 'no-cors'})
+//     // .then( res => {
+//     //     //console.log(res);
+//     //     if(res.ok ){
+//     //         return res
+//     //     }else {
+//     //         errorTitle = "Błąd "+ res.status;
+//     //         errorText = res.statusText;
+//     //     }
+//     //     throw Error(res.status + " - Połączenie")
+//     // })
+//     // .then( (res) => {
+//     //     //console.log("JSON");
+//     //     return res.json();
+//     // })
+//     // .then( (data) => {
+//     //     //console.log(data, displayWeather);
+//     //     //console.log("DATA");
+//     //     console.log(data)
+//     //     autocomplete.innerHTML = "";
+//     //     data.forEach(element => {
+//     //         if(element.Country.ID === "PL"){
+//     //             //console.log(element.LocalizedName+", "+element.AdministrativeArea.LocalizedName);
+//     //             let li = document.createElement("li");
+//     //             li.innerHTML = element.LocalizedName+", "+element.AdministrativeArea.LocalizedName;
+//     //             li.addEventListener("click",()=>{
+//     //                 // showCityWeather(element.Key)
+//     //                 //console.log(element)
+//     //                 //showCityWeather(element.Key)
+//     //             })
+//     //             autocomplete.appendChild(li);
+//     //         }
+//     //     });
+//     //     //setDisplayWeather(data);
+//     // })
+//     // .catch(err => {
+//     //     displayError(errorTitle||"Błąd aplikacji", errorText + " " + err)
+//     //     console.log(err);
+//     // });
+//     const options = {
+//         method: 'GET',
+//         headers: {
+//             'X-RapidAPI-Key': '94cb7370bamshbcd007e451dee8cp164e08jsnbb844ff17ca6',
+//             'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+//         }
+//     };
+    
+//     fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/cities?countryIds=pl&sort=population&languageCode=en&minPopulation=500&asciiMode=true&types=CITY&namePrefix='+city, options)
+//         .then(response => response.json())
+//         .then(response => {
+//             console.log(response)
+//             autocomplete.innerHTML = "";
+//             response.data.forEach(element => {
+//                 let li = document.createElement("li");
+//                 li.innerHTML = element.city+", "+element.region;
+//                 li.addEventListener("click",()=>{
+//                     showCityWeather(element.city)
+//                 })
+//                 autocomplete.appendChild(li);
+            
+//             });
+//         })
+//         .catch(err => console.error(err));
+// };
 
 searchButton.addEventListener("click", ()=>{
-    //showCityWeather(searchInput.value)
+    showCityWeather(searchInput.value);
+    searchInput.value="";
 });
 
 let setDisplayWeather = (data) => {
@@ -105,14 +155,48 @@ document.querySelector('.error-container button').addEventListener("click", ()=>
 })
 
 }
-searchInput.addEventListener("input", (e)=>{
-    if(searchInput.value.length > 1){
-        //showCitylist(searchInput.value);
+
+
+// searchInput.addEventListener("input", (e)=>{
+//     if(searchInput.value.length > 1){
+//         showCitylist(searchInput.value);
+//     }
+// })
+// searchInput.addEventListener("click", ()=>{
+//     searchWrap.classList.remove("close")
+// })
+// closeAutosearchButton.addEventListener("click", ()=>{
+//     searchWrap.classList.add("close")
+// })
+
+let setCityDetails = (city) => {
+    console.log("city")
+    console.log(city);
+    cityName.innerHTML = city.name;
+    let dateNow = new Date(city.dt *1000);
+    cityDate.innerHTML = dateNow.getFullYear()+"-"+ (dateNow.getMonth()+1) +"-"+dateNow.getDate();
+    dateNow = new Date(city.sys.sunrise *1000);
+    cityDayStart.innerHTML = dateNow.getHours()+":"+ (String(dateNow.getMinutes()).length>1?dateNow.getMinutes():"0"+dateNow.getMinutes());
+    dateNow = new Date(city.sys.sunset *1000);
+    cityDayEnd.innerHTML = dateNow.getHours()+":"+ (String(dateNow.getMinutes()).length>1?dateNow.getMinutes():"0"+dateNow.getMinutes());
+    console.log(city.weather.icon);
+    cityIcon.src = "http://openweathermap.org/img/wn/"+city.weather[0].icon+"@2x.png";
+    cityIcon.alt = city.weather[0].description;
+    cityTemp.innerHTML = Math.round(city.main.temp)+"°C";
+    cityDesc.innerHTML = city.weather[0].description;
+    cityTempFeel.innerHTML = Math.round(city.main.feels_like)+"°C";
+    cityPressure.innerHTML = city.main.pressure+" hPa";
+    cityWind.innerHTML = city.wind.speed +" km/h";
+} 
+
+
+function activeteCitySearch(){
+    let option = {
+        types: ['(cities)'],
+        componentRestrictions: { country: ["pl"],}
     }
-})
-searchInput.addEventListener("click", ()=>{
-    searchWrap.classList.remove("close")
-})
-closeAutosearchButton.addEventListener("click", ()=>{
-    searchWrap.classList.add("close")
-})
+    new google.maps.places.Autocomplete(searchInput, option);
+}
+//setCityDetails(showCityWeather("Warszawa"));
+console.log("Warszawa");
+showCityWeather("Warszawa");
